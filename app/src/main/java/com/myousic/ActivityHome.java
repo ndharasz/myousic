5a49ab085a1a571bb4648d3524bad90ca2571552
Noah Harasz <Noah Harasz>
2017-02-09 17:31:44 -0500

    initial commit, home screen, spotify sdk setup and app link

---
+++ app/src/main/java/com/myousic/ActivityHome.java
@@ -0,0 +1,28 @@
+package com.myousic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.ndharasz.myousic.R;
+import com.spotify.sdk.android.authentication.LoginActivity;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    protected void create(View v) {
+        Intent createPartyIntent = new Intent(this, LoginActivity.class);
+        startActivity(createPartyIntent);
+    }
+
+    protected void join(View v) {
+        Intent joinPartyIntent = new Intent(this, ActivityJoinParty.class);
+        startActivity(joinPartyIntent);
+    }
+}
---
+++ app/src/main/java/com/myousic/ActivityJoinParty.java
@@ -0,0 +1,15 @@
+package com.myousic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ndharasz.myousic.R;
+
+public class ActivityJoinParty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_party);
+    }
+}
