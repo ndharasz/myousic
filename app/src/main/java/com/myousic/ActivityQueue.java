bd122d45fdd5641aa23bb735d9930a59cfa3dc5d
bwoodbury3 <brianj.woodbury@gmail.com>
2017-02-09 18:54:55 -0500

    ID-entering and queue screen

---
+++ app/src/main/java/com/myousic/ActivityQueue.java
@@ -0,0 +1,15 @@
+package com.myousic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ndharasz.myousic.R;
+
+public class ActivityQueue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queue);
+    }
+}
