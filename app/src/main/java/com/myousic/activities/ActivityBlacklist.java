1304aa2f64f5b6331d01c6ed575da86a77368361
bwoodbury3 <brianj.woodbury@gmail.com>
2017-07-10 18:13:24 -0400

    rebasing

--- app/src/main/java/com/myousic/activities/ActivityBlacklist.java
+++ app/src/main/java/com/myousic/activities/ActivityBlacklist.java
@@ -1,20 +1,25 @@
 package com.myousic.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.myousic.R;
+import com.myousic.util.BlacklistController;
 
 public class ActivityBlacklist extends AppCompatActivity {
+    BlacklistController blacklistController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blacklist);
+        String partyID = getSharedPreferences("Party", Context.MODE_PRIVATE).getString("party_id", null);
+        blacklistController = new BlacklistController(partyID);
     }
 
     protected void addSong(View v) {
-
+        // Pop up dialog with search or new page.  Either or.
     }
 }
